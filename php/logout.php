1fd7e736658779741777cfdef0e71fc5f516e709
Toomas <toomas.kirsing@gmail.com>
2023-03-24 22:22:33 +0200

    Fix logout redirect to index.php from .html

--- php/logout.php
+++ php/logout.php
@@ -3,4 +3,4 @@ session_set_cookie_params(0);
 session_start(); // Start a session
 $_SESSION["logged_in"] = false; // Unset the session variable
 session_destroy();
-header('Location: ../index.html');
+header('Location: ../index.php');
