3dc6d3a09a66d3e6c33b0f4acf2ee43acc918297
Toomas <toomas.kirsing@gmail.com>
2023-03-24 23:41:35 +0200

    Move text config files to sqlite database

--- config.php
+++ config.php
@@ -1,7 +1,25 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'my_database');
-define('DB_USER', 'my_username');
-define('DB_PASSWORD', 'my_password');
+define('DB_FILE', 'db/database.db');
 ini_set('display_errors', 1);
 error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED);
+
+try {
+    $pdo = new PDO('sqlite:' . DB_FILE);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
+
+$sql = "CREATE TABLE settings (
+    password TEXT NOT NULL,
+    url TEXT NOT NULL,
+    timezone TEXT NOT NULL
+)";
+
+// Execute the SQL statement to create the table
+try {
+$pdo->exec($sql);
+echo "Table 'users' created successfully.";
+} catch (PDOException $e) {
+die("Table creation failed: " . $e->getMessage());
+}
