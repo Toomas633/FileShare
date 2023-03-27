b8de171f495145cbcb50cf10d876ee4161982a53
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-03-27 13:16:21 +0300

    Fix settings file list download script

--- php/settings-download.php
+++ php/settings-download.php
@@ -6,13 +6,14 @@ if (isset($_POST['filename'])) {
     $filename = basename($file);
     if (!file_exists($filepath)) {
         $status = "File not found.";
-        header("Location: " . DIR_PATH . "settings.php?status=" . urlencode($status));
+        header("Location: ../settings.php?status=" . urlencode($status));
+        exit();
+    } else {
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . $filepath . '"');
+        header('Content-Length: ' . filesize($filepath));
+        readfile($filepath);
+        header("Location: ../settings.php?status=" . urlencode($status));
         exit();
     }
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . $filename . '"');
-    header('Content-Length: ' . filesize($filepath));
-    readfile($filepath);
-    header("Location: ../settings.php?status=" . urlencode($status));
-    exit();
 }
