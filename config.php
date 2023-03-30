5f39cd8ffd34303f7c72768230e7b363bcc66c0f
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-03-30 11:51:41 +0300

    Remove relative db path

--- config.php
+++ config.php
@@ -1,6 +1,13 @@
 <?php
+$current_file_path = __FILE__;
+$current_file_dir = dirname($current_file_path);
+chdir($current_file_dir);
 define('DEFAULT_PASS', '$2y$10$Wd3nh6Kg6bRv6TpKz0E5eOrvONkObd7JhQmkeFV2QbVOHZqDSfkkK');
 define('DEFAULT_TZ', 'Europe/London');
+define('DIR_PATH', $current_file_dir . '/');
+define('DB_FILE', $current_file_dir . '/db/database.db');
+error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 0);
 
 if (!file_exists("uploads/")) {
     mkdir("uploads/", 0777, true);
@@ -9,7 +16,7 @@ if (!file_exists("db/")) {
     mkdir("db/", 0777, true);
 }
 
-if (!file_exists('db/database.db')) {
+if (!file_exists(DB_FILE)) {
     if (getenv('PASSWORD') !== false) {
         $password = password_hash(getenv('PASSWORD'), PASSWORD_BCRYPT);
     } else {
@@ -20,7 +27,7 @@ if (!file_exists('db/database.db')) {
     } else {
         $tz = DEFAULT_TZ;
     }
-    $pdo = new PDO('sqlite:db/database.db');
+    $pdo = new PDO('sqlite:' . DB_FILE);
     $pdo->exec("CREATE TABLE files (name TEXT, uploadTime INT, deleteTime INT)");
     $pdo->exec("CREATE TABLE settings (setting TEXT, value TEXT)");
     $pdo->exec("INSERT INTO settings (setting, value) VALUES ('password', '$password')");
@@ -28,11 +35,3 @@ if (!file_exists('db/database.db')) {
     $pdo->exec("INSERT INTO settings (setting, value) VALUES ('url', 'http://localhost:8000/')");
     $pdo = null;
 }
-
-$current_file_path = __FILE__;
-$current_file_dir = dirname($current_file_path);
-chdir($current_file_dir);
-define('DIR_PATH', $current_file_dir . '/');
-define('DB_FILE', $current_file_dir . '/db/database.db');
-error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('display_errors', 0);
