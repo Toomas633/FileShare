90dc563f6e0bbeffdb23464bce1b93dbcfb82d78
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-04-01 15:46:51 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('path.php');
 define('DIR_PATH', DIR_PATH. '/');
 define('DB_FILE', DIR_PATH . '/db/database.db');
-error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('display_errors', 0);
+//error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 1);
 function changePathOnFirstRun() {
     static $firstRun = true;
     if ($firstRun) {
