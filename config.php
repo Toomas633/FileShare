721abacea6a8e2676eb769e0d79cc267e344112d
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-04-01 18:29:50 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
 require_once('path.php');
-define('DB_FILE', DIR_PATH . '/db/database.db');
+define('DB_FILE', DEFAULT_PATH . '/db/database.db');
+define('DIR_PATH', DEFAULT_PATH . '/');
 //error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
 ini_set('display_errors', 1);
+
+if (!file_exists('uploads/')) {
+    mkdir('uploads/', 0777, true);
+}
+
+if (!file_exists('db/')) {
+    mkdir('db/', 0777, true);
+}
