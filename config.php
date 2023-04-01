c4c70fbcd68f7acbd2022eda08511c6e12d77172
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-04-01 15:59:15 +0300

    Fix path variable

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 require_once('path.php');
-define('DIR_PATH', DIR_PATH. '/');
 define('DB_FILE', DIR_PATH . '/db/database.db');
 //error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
 ini_set('display_errors', 1);
@@ -14,15 +13,15 @@ function changePathOnFirstRun() {
         if (!file_exists("db/")) {
             mkdir("db/", 0777, true);
         }
-        createDB(DIR_PATH);
+        createDB();
         $firstRun = false;
     }
 }
 
-function createDB($current_file_dir) {
+function createDB() {
     define('DEFAULT_PASS', '$2y$10$Wd3nh6Kg6bRv6TpKz0E5eOrvONkObd7JhQmkeFV2QbVOHZqDSfkkK');
     define('DEFAULT_TZ', 'Europe/London');
-    if (!file_exists($current_file_dir . '/db/database.db')) {
+    if (!file_exists(DIR_PATH . '/db/database.db')) {
         if (getenv('PASSWORD') !== false) {
             $password = password_hash(getenv('PASSWORD'), PASSWORD_BCRYPT);
         } else {
@@ -34,7 +33,7 @@ function createDB($current_file_dir) {
             $tz = DEFAULT_TZ;
         }
         try {
-            $db = new PDO('sqlite:' . $current_file_dir . '/db/database.db');
+            $db = new PDO('sqlite:' . DIR_PATH . '/db/database.db');
             $query=$db->prepare("CREATE TABLE files (name TEXT, uploadTime INT, deleteTime INT)");
             $query->execute();
             $db = $query = null;
@@ -42,7 +41,7 @@ function createDB($current_file_dir) {
             echo 'Error: Unable to create table files';
         }
         try {
-            $db = new PDO('sqlite:' . $current_file_dir . '/db/database.db');
+            $db = new PDO('sqlite:' . DIR_PATH . '/db/database.db');
             $query=$db->prepare("CREATE TABLE settings (setting TEXT, value TEXT)");
             $query->execute();
             $db = $query = null;
@@ -50,7 +49,7 @@ function createDB($current_file_dir) {
             echo 'Error: Unable to create table settings';
         }
         try {
-            $db = new PDO('sqlite:' . $current_file_dir . '/db/database.db');
+            $db = new PDO('sqlite:' . DIR_PATH . '/db/database.db');
             $query=$db->prepare("INSERT INTO settings (setting, value) VALUES ('password', :password)");
             $query->bindValue(':password', $password, PDO::PARAM_STR);
             $query->execute();
@@ -59,7 +58,7 @@ function createDB($current_file_dir) {
             echo 'Error: Unable to insert password to table settings';
         }
         try {
-            $db = new PDO('sqlite:' . $current_file_dir . '/db/database.db');
+            $db = new PDO('sqlite:' . DIR_PATH . '/db/database.db');
             $query=$db->prepare("INSERT INTO settings (setting, value) VALUES ('timezone', :tz)");
             $query->bindValue(':tz', $tz, PDO::PARAM_STR);
             $query->execute();
@@ -68,7 +67,7 @@ function createDB($current_file_dir) {
             echo 'Error: Unable to insert timezone to table settings';
         }
         try {
-            $db = new PDO('sqlite:' . $current_file_dir . '/db/database.db');
+            $db = new PDO('sqlite:' . DIR_PATH . '/db/database.db');
             $query=$db->prepare("INSERT INTO settings (setting, value) VALUES ('url', :url)");
             $query->bindValue(':url', 'http://localhost:8000/', PDO::PARAM_STR);
             $query->execute();
--- path.php
+++ path.php
@@ -1,4 +1,4 @@
 <?php
 $current_file_path = __FILE__;
 $current_file_dir = dirname($current_file_path);
-define('DIR_PATH', $current_file_dir);
+define('DIR_PATH', $current_file_dir . '/');
