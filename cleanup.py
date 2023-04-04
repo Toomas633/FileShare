d34702d384cd32d78b60088c03d35b42de6f786c
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-04-04 16:47:59 +0300

    Make python log colorful

--- cleanup.py
+++ cleanup.py
@@ -3,10 +3,9 @@ import os
 import sqlite3
 import time
 from datetime import datetime
-
 import pytz
 
-logging.basicConfig(level=logging.DEBUG, filename='cleanup.log', filemode='w', format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename='cleanup.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
 
 def deleteEntries():
     conn = sqlite3.connect('db/database.db')
@@ -22,6 +21,7 @@ def deleteEntries():
     cursor.close()
     conn.close()
 
+
 def deleteFiles():
     conn = sqlite3.connect('db/database.db')
     cursor = conn.cursor()
@@ -30,18 +30,35 @@ def deleteFiles():
     current_time = int(datetime.now(pytz.timezone('UTC')).timestamp() * 1000)
     for file in files:
         name = file[0]
-        upload_time=file[1]
+        upload_time = file[1]
         delete_time = file[2]
         if current_time > delete_time and delete_time != upload_time:
             file_path = os.path.join('uploads/', name)
             if os.path.exists(file_path):
-                logging.info(f"Deleted file: {name} \t Delete time in db(utc): {delete_time}")
+                logging.info(
+                    f"Deleted file: {name} \t Delete time in db(utc): {delete_time}")
                 os.remove(file_path)
     cursor.close()
     conn.close()
 
+
 if __name__ == '__main__':
+    logging.debug('Started cleanup.py')
+    
+    # Log some messages
+    logging.debug('This is a debug message')
+    logging.info('This is an info message')
+    logging.warning('This is a warning message')
+    logging.error('This is an error message')
+    logging.critical('This is a critical message')
+
     while True:
-        deleteFiles()
-        deleteEntries()
+        try:
+            deleteFiles()
+        except Exception as e:
+            logging.error(e)
+        try:
+            deleteEntries()
+        except Exception as e:
+            logging.error(e)
         time.sleep(60)
