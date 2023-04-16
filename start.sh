e3a5ad2ca4721d3350d646c9456b3b00772a4f72
Toomas Kirsing <toomas.kirsing@gmail.com>
2023-04-16 21:40:09 +0300

    Add a bash script for running the server

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function cleanup {
+  echo "Cleaning up child processes"
+  pkill -P $$
+}
+trap 'cleanup' SIGTERM
+php createDB.php > FileShare.log &
+php -S 0.0.0.0:8000 > FileShare.log &
+python3 cleanup.py > cleanup.log &
+wait
