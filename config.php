2eff95e6a86da4cfbb032bd21072424408cea944
Toomas <toomas.kirsing@gmail.com>
2023-04-29 00:40:28 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,4 +5,3 @@ define('DIR_PATH', DEFAULT_PATH . '/');
 error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
 ini_set('log_errors', 1);
 ini_set('error_log', DEFAULT_PATH . '/FileShare.log');
-ini_set('include_path', DEFAULT_PATH . 'FileShare.ini');
