587460b2790832de0c7fc41b98df34d3f9d391c2
Toomas <toomas.kirsing@gmail.com>
2023-06-14 17:22:31 +0300

    Update timeouts

--- config.php
+++ config.php
@@ -6,5 +6,4 @@ require_once 'path.php';
 define('DB_FILE', DEFAULT_PATH . '/db/database.db');
 define('DIR_PATH', DEFAULT_PATH . '/');
 error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('log_errors', 1);
 ini_set('error_log', DEFAULT_PATH . '/FileShare.log');
