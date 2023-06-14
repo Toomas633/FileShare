3b4d702289bc15d70f87535c6f877ba6c4cf7de4
Toomas <toomas.kirsing@gmail.com>
2023-06-14 16:14:43 +0300

    Add CORS headers

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'config.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
 ?>
 <!DOCTYPE html>
 <html lang="eng">
--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,8 @@
 <?php
 require_once '../config.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 $targetDir = DIR_PATH . "uploads/";
 $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 $random = intval($_POST['random']);
