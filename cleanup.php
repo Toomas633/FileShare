de71619d84b57c4a09e283375d5302f117672f46
Toomas <toomas.kirsing@gmail.com>
2023-06-27 16:30:59 +0300

    Link cleanup and db check to config.php

--- cleanup.php
+++ cleanup.php
@@ -1,43 +1,44 @@
 <?php
-ini_set('error_log', DEFAULT_PATH . '/cleanup.log');
+require_once 'path.php';
 date_default_timezone_set('UTC');
+ini_set('error_log', DIR_PATH . '/cleanup.log');
 
 function delete_entries()
 {
-    $conn = new SQLite3('db/database.db');
-    $files_in_folder = scandir("uploads/");
+    $conn = new PDO('sqlite:' . DB_FILE);;
+    $files_in_folder = scandir(DIR_PATH . "uploads/");
     $files_in_database = [];
     $result = $conn->query("SELECT name FROM files");
-    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         $files_in_database[] = $row['name'];
     }
     foreach ($files_in_database as $filename) {
         if (!in_array($filename, $files_in_folder)) {
             $conn->exec("DELETE FROM files WHERE name = '$filename'");
-            error_log("Deleted entry for file: $filename", 3, 'cleanup.log');
+            error_log("Deleted entry for file: $filename", 3, DIR_PATH . '/cleanup.log');
         }
     }
-    $conn->close();
+    $conn = null;
 }
 
 function delete_files()
 {
-    $conn = new SQLite3('db/database.db');
+    $conn = new PDO('sqlite:' . DB_FILE);;
     $result = $conn->query("SELECT * FROM files");
     $current_time = time() * 1000;
-    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         $name = $row['name'];
         $upload_time = $row['upload_time'];
         $delete_time = $row['delete_time'];
         if ($current_time > $delete_time && $delete_time != $upload_time) {
-            $file_path = 'uploads/' . $name;
+            $file_path = DIR_PATH . 'uploads/' . $name;
             if (file_exists($file_path)) {
-                error_log("Deleted file: $name \t Delete time in db(utc): $delete_time", 3, 'cleanup.log');
+                error_log("Deleted file: $name \t Delete time in db(utc): $delete_time", 3, DIR_PATH . '/cleanup.log');
                 unlink($file_path);
             }
         }
     }
-    $conn->close();
+    $conn = null;
 }
 
 echo "Started cleanup.php\n";
