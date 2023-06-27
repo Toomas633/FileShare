2f4330cf7f2d466669bb93258148e9351f1f1c76
Toomas <toomas.kirsing@gmail.com>
2023-06-27 21:08:29 +0300

    Update cleanup.php

--- cleanup.php
+++ cleanup.php
@@ -7,19 +7,34 @@ ini_set('error_log', 'cleanup.log');
 
 function delete_entries()
 {
-    $conn = new PDO('sqlite:' . DB_FILE);;
-    $files_in_folder = scandir("uploads/");
-    $files_in_database = [];
-    $result = $conn->query("SELECT name FROM files");
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $files_in_database[] = $row['name'];
+    $conn = new PDO('sqlite:' . DB_FILE);
+    $files_in_folder = array();
+    $files_in_database = array();
+    if (is_dir("uploads/")) {
+        $directory = new DirectoryIterator("uploads/");
+        foreach ($directory as $file) {
+            if ($file->isFile()) {
+                $files_in_folder[] = $file->getFilename();
+            }
+        }
+    }
+    try {
+        $pdo = new PDO("sqlite:" . DB_FILE);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = "SELECT name FROM files";
+        $stmt = $pdo->query($query);
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $files_in_database[] = $row['name'];
+        }
+    } catch (PDOException $e) {
+        echo $e;
     }
     foreach ($files_in_database as $filename) {
         if (!in_array($filename, $files_in_folder)) {
             $stmt = $conn->prepare("DELETE FROM files WHERE name = :filename");
             $stmt->bindParam(':filename', $filename);
             $stmt->execute();
-            error_log("Deleted entry for file: $filename", 3, 'cleanup.log');
+            error_log("Deleted entry for file:" . $filename . PHP_EOL, 3, 'cleanup.log');
         }
     }
     $conn = null;
@@ -27,20 +42,24 @@ function delete_entries()
 
 function delete_files()
 {
-    $conn = new PDO('sqlite:' . DB_FILE);;
-    $result = $conn->query("SELECT * FROM files");
-    $current_time = time() * 1000;
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $name = $row['name'];
-        $upload_time = $row['upload_time'];
-        $delete_time = $row['delete_time'];
-        if ($current_time > $delete_time && $delete_time != $upload_time) {
-            $file_path ='uploads/' . $name;
-            if (file_exists($file_path)) {
-                error_log("Deleted file: $name \t Delete time in db(utc): $delete_time", 3, 'cleanup.log');
-                unlink($file_path);
+    try {
+        $conn = new PDO('sqlite:' . DB_FILE);;
+        $stmt = $conn->query("SELECT * FROM files");
+        $current_time = time() * 1000;
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $name = $row['name'];
+            $upload_time = $row['uploadTime'];
+            $delete_time = $row['deleteTime'];
+            if ($current_time > $delete_time && $delete_time != $upload_time) {
+                $file_path = 'uploads/' . $name;
+                if (file_exists($file_path)) {
+                    error_log("Deleted file: " . $name . "\t Delete time in db(utc): " . $delete_time . PHP_EOL, 3, 'cleanup.log');
+                    unlink($file_path);
+                }
             }
         }
+    } catch (PDOException $e) {
+        echo $e;
     }
     $conn = null;
 }
